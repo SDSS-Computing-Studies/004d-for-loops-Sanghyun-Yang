a927d830829c1d655159a4736df69818f5b82051
Sanghyun-Yang <78501215+Sanghyun-Yang@users.noreply.github.com>
2021-02-19 09:36:01 -0800

    Update task2.py

--- task2.py
+++ task2.py
@@ -23,3 +23,10 @@ That name is on the list
 """
 
 nameList = ("Lebron","Kobe","Michale","Shaq","Dennis")
+
+x = str(input())
+
+if x in nameList:
+    print("That name is on the list")
+else:
+    print("That is not on the list")
