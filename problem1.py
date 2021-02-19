cfeb48192c58e8cdde9b4ba52d9e2bdc01b9c6e4
Sanghyun-Yang <78501215+Sanghyun-Yang@users.noreply.github.com>
2021-02-19 10:09:25 -0800

    Update problem1.py

--- problem1.py
+++ problem1.py
@@ -4,7 +4,7 @@
 Ask the user to enter in the width and height of a box.
 This should be an integer value less than 10
 Draw a box filled with "*" symbols that matches the
-width and height.
+width and heigsht.
 You will need 2 nested loops to draw the contents of
 1 row and the number of rows.
 
